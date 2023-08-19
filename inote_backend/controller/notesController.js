d5d3644f99c03bda0131ebe75400181d76a57dee
Prathyarthi <Prathyarthi@prathyarthi.com>
2023-08-19 16:58:46 +0530

    Added update,delete routes

--- inote_backend/controller/notesController.js
+++ inote_backend/controller/notesController.js
@@ -39,8 +39,67 @@ const addNote = async (req, res) => {
 
 }
 
+const updateNote = async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+        // Create a newNote object
+        const newNote = {};
+        if (title) {
+            newNote.title = title;
+        }
+        if (description) {
+            newNote.description = description;
+        }
+        if (tag) {
+            newNote.tag = tag;
+        }
+
+        // Find the notes to updated and update it
+        let note = await notesModel.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found");
+        }
+
+        if (note.user.toString() !== req.user.id) {         // To convert id which is object into string and compare
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await notesModel.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
+    } catch (e) {
+        return res.status(400).json({
+            success: false,
+            message: e.message
+        })
+    }
+}
+
+const deleteNote = async (req, res) => {
+    try {
+        // Find the notes to deleted and delete it
+        let note = await notesModel.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found");
+        }
+
+        // Allow deletion only if the user owns this note 
+        if (note.user.toString() !== req.user.id) {         // To convert id which is object into string and compare
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await notesModel.findByIdAndDelete(req.params.id);
+        res.json({ "success": "Note has been deleted!" });
+    } catch (e) {
+        return res.status(400).json({
+            success: false,
+            message: e.message
+        })
+    }
+}
 
 module.exports = {
     fetchAllNotes,
-    addNote
+    addNote,
+    updateNote,
+    deleteNote
 }
--- inote_backend/routes/notes.js
+++ inote_backend/routes/notes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fetchAllNotes, addNote } = require("../controller/notesController");
+const { fetchAllNotes, addNote, updateNote, deleteNote } = require("../controller/notesController");
 const fetchuser = require('../middleware/fetchuser');
 const { body, validationResult } = require('express-validator');
 var router = express.Router();
@@ -9,5 +9,8 @@ router.post('/addNote', fetchuser, [
     body('title', "Enter a valid title").isLength({ min: 3 }),
     body('description', "description must be minimum 5 characters").isLength({ min: 5 }),
 ], addNote);
+
+router.put('/updateNote/:id', fetchuser, updateNote);
+router.delete('/deleteNote/:id', fetchuser, deleteNote);
 
 module.exports = router;
